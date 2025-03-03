983b8853ccc966e7a657608b5970795aeff72d3b
Nishant407 <nishant407.ns@gmail.com>
2025-03-03 16:51:20 +0530

    UC1

---
+++ src/main/java/com/example/GreetingApp/service/IGreetingService.java
@@ -0,0 +1,9 @@
+package com.example.GreetingApp.service;
+
+import com.example.GreetingApp.model.Greeting;
+import com.example.GreetingApp.model.User;
+
+public interface IGreetingService {
+    Greeting addGreeting(User user);
+    Greeting getGreetingById(long id);
+}
